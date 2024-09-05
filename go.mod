b629ced91d520d7152f0bbe65a6f30d51baf562c
PC <lucasmoreau38m@gmail.com>
2024-09-05 18:30:04 +0200

    add first fetchURL func

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module gogole
 
 go 1.21
+
+require github.com/PuerkitoBio/goquery v1.9.2
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,41 @@
-package gogole
+package main
 
-func fetchURL(url string) {
-	// fetch URL
-}
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func extractLinks() {
-	// extract links
+	"github.com/PuerkitoBio/goquery"
+)
+
+func fetchURL() {
+	// Request the HTML page.
+	res, err := http.Get("http://158.178.197.230:8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	doc.Find("a[href]").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the title
+		link, ok := s.Attr("href")
+		if !ok {
+			return
+		}
+		fmt.Println(link)
+	})
 }
 
 func main() {
-	// main function with url paramete
+	fetchURL()
 }
